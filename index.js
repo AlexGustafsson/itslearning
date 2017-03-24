c8243c4f9acacebb4f79e050f84d230cf31b8f4f
Alex Gustafsson <alex-gustafsson@live.se>
2017-03-24 15:19:13 +0100

    Removed debug

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 const ItsLearning = require('./lib/itslearning');
 
-ItsLearning.searchOrganisation('Blekinge tekniska högskola')
-.then(ItsLearning.fetchOrganisation)
-.then(organisation => {
-  console.log(typeof organisation.id);
-});
-
 module.exports = ItsLearning;
